08d803af7f07101890e8e797364eea2c921f34c3
Bernal, Daniel <Daniel.Bernal@nrel.gov>
2024-09-10 09:54:27 -0600

    updated Manifest.toml and Project.toml

---
+++ HPC_run.jl
@@ -0,0 +1,12 @@
+"""
+This file contains the script to run REopt on the HPC.
+
+This is not meant to run the full optimized techo-economic capabilities of REopt.
+We're only doing technical potential so the only real inputs are 
+    A) space availability, (constraint)
+    B) load profile, (hopefully scaled to the specific industrial sector)
+        i. electrical,
+        ii. thermal.
+    C) lat/long.
+"""
+
