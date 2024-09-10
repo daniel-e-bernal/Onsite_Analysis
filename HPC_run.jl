aa28542df4fe358a9a8c51e3e53579aa45407d42
Bernal, Daniel <Daniel.Bernal@nrel.gov>
2024-09-10 12:39:46 -0600

    installed PyCall in case

--- HPC_run.jl
+++ HPC_run.jl
@@ -10,3 +10,8 @@ We're only doing technical potential so the only real inputs are
     C) lat/long.
 """
 
+using REopt, DataFrames, XLSX, JSON, DeliminatedFiles
+
+#also potentially using PyCall to call on the datasets that are within the HPC storage such as NSRDBX and WINDToolkit
+#using PyCall
+
